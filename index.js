ca560e608c554876a424f90edda17315785eba5a
eevaristo <elton.evaristo@gmail.com>
2021-12-31 22:15:31 -0300

    Classes abstratas js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import { ContaSalario } from "./ContaSalario.js";
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
@@ -6,14 +8,9 @@ const cliente1 = new Cliente("Ricardo", 11122233309);
 
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001)
-
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
-contaPoupanca.sacar(10);
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(100);
+contaSalario.sacar(10);
 
-
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
-
+console.log(contaSalario);
