d80149c02cc59be5e994fbb332adc3ade05a3f4f
eevaristo <elton.evaristo@gmail.com>
2021-12-31 21:39:32 -0300

    Estudos de JavaScript

---
+++ ContaCorrente.js
@@ -0,0 +1,23 @@
+import { Conta } from "./Conta.js";
+
+export class ContaCorrente extends Conta {
+    static numeroDeContas = 0;
+    constructor(cliente, agencia) {
+        super(0, cliente, agencia);
+        ContaCorrente.numeroDeContas += 1;
+    }
+    
+    teste(){
+        console.log("testa na classe conta corrente");
+    }
+
+    sacar(valor) {
+        let taxa = 1.1;
+        const valorSacado = taxa * valor;
+        if(this._saldo >= valorSacado){
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+    }
+
+}
